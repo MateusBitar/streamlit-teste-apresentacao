ffd0c305197a4536f074442fa2dc39ac664af9cb
MateusBitar <129760150+MateusBitar@users.noreply.github.com>
2024-01-03 11:18:48 -0300

    Update teste_login.py

--- teste_login.py
+++ teste_login.py
@@ -2,10 +2,12 @@ import pandas as pd
 import streamlit as st
 
 
-df = pd.DataFrame(tenistas.csv)
+df = pd.read_csv("tenistas.csv")
 
 df.columns = ["nome", "usuario", "senha", "rank", "apelido" ]
 
+novalinha = {"nome": "Georges Bitar", "usuario" : "Georges", "senha" : "senha", "rank": 5, "apelido": "Papai Georges" }
+df = pd.concat([df, pd.DataFrame([novalinha])], ignore_index=True)
 
 def main():
     st.title("Tela de Login")
@@ -13,15 +15,18 @@ def main():
     # Criando campos de entrada para usuário e senha
     username = st.text_input("Usuário")
     password = st.text_input("Senha", type="password")
-
+    
     # Verificando se o botão de login foi pressionado
     if st.button("Login"):
-        # Simples verificação de usuário e senha (substitua isso por sua lógica de autenticação real)
-        if username == df[usuario] and password == df[senha]:
+        # Simples verificação de usuário e senha
+        if not tenistas_df.empty and \
+           (tenistas_df['usuario'] == username).any() and \
+           (tenistas_df['senha'] == password).any():
             st.success("Login bem-sucedido!")
             # Adicione aqui o redirecionamento ou ações que você deseja após o login
         else:
             st.error("Usuário ou senha incorretos.")
+
 
 if __name__ == "__main__":
     main()
