4cadc4787f7cdc51f4c79a6bf2da7e42ba10b80b
MateusBitar <129760150+MateusBitar@users.noreply.github.com>
2024-01-13 00:50:13 -0300

    Update Página-Principal.py

--- Página-Principal.py
+++ Página-Principal.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 def main():
     st.title("Bem Vindo ao ATP Peninsula")
-    st.write("Para saber um pouco mais do nosso regulamento clique aqui: (regulamentopeninsula-oez9g6dwh6se9qku94e58k.streamlit.app/?){:target='_blank'}")
+    st.write("[Para saber um pouco mais do nosso regulamento clique aqui:](https://regulamentopeninsula.streamlit.app){:target='_blank'}")
 
 
 if __name__ == "__main__":
