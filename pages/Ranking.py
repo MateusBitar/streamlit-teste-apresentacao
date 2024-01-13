6fd3e4ac92a4d9f050736299711f697c3a69b37b
MateusBitar <129760150+MateusBitar@users.noreply.github.com>
2024-01-13 01:21:27 -0300

    Update Ranking.py

--- pages/Ranking.py
+++ pages/Ranking.py
@@ -31,3 +31,8 @@ st.dataframe(
         },
     hide_index=True,
     )
+
+ df = df.drop(df.rank[6])
+    
+    # Escrever o DataFrame de volta para o arquivo CSV
+    df.to_csv(nome_arquivo, index=False)
