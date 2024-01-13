9a634c71cd8b2684cd77b12f31afe32e39f43d8a
MateusBitar <129760150+MateusBitar@users.noreply.github.com>
2024-01-13 01:11:37 -0300

    Update Página-Principal.py

--- Página-Principal.py
+++ Página-Principal.py
@@ -2,7 +2,18 @@ import streamlit as st
 import pandas as pd
 def main():
     st.title("Bem Vindo ao ATP Peninsula")
-    st.write("{{Para saber um pouco mais do nosso regulamento clique}}[aqui:](https://regulamentopeninsula.streamlit.app)){{:target='_blank'}}")
+
+    frase = "Para saber um pouco mais do nosso regulamento clique"
+    
+    # Texto do link e URL
+    link_text = "aqui"
+    link_url = "https://regulamentopeninsula.streamlit.app"
+    
+    # Utilizando .format() para formatar a string
+    texto_completo = "{} [{}]({}){{:target='_blank'}}".format(frase, link_text, link_url)
+    
+    # Exibindo no Streamlit
+    st.write(texto_completo)
 
 
 if __name__ == "__main__":
