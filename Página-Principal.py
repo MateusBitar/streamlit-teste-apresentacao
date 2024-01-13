26158e67a450fea2e06f9aea402eb3394aca5098
MateusBitar <129760150+MateusBitar@users.noreply.github.com>
2024-01-13 00:59:30 -0300

    Update Página-Principal.py

--- Página-Principal.py
+++ Página-Principal.py
@@ -4,7 +4,6 @@ def main():
     st.title("Bem Vindo ao ATP Peninsula")
     link_text = "Para saber um pouco mais do nosso regulamento clique aqui:"
     link_url = "https://regulamentopeninsula.streamlit.app){:target='_blank'"
-    st.markdown(f"[{link_text}]({link_url}){:target='_blank'}")
 
     # Ou usando concatenação de strings
     st.write("[{}]({}){:target='_blank'}".format(link_text, link_url))
