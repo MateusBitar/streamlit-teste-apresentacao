08a8b2ab42aa14a4ed7ea11c530f06059035ddfa
MateusBitar <129760150+MateusBitar@users.noreply.github.com>
2024-01-13 00:41:12 -0300

    Update and rename main.py to Página-Principal.py

---
+++ Página-Principal.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+def main():
+    st.title("Bem Vindo ao ATP Peninsula")
+    st.write("Para saber um pouco mais do nosso regulamento clique aqui: (regulamentopeninsula-oez9g6dwh6se9qku94e58k.streamlit.app/?)")
+
+
+if __name__ == "__main__":
+    main()
